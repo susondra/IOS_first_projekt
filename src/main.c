e06261ffeb981cde13e74fba31af0b05026264cd
susondra <123081753+susondra@users.noreply.github.com>
2024-03-11 19:18:51 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+//  for projekt:
+//  gcc -std=c11 -Wall -Wextra -Werror src/maze.c -o maze
+
+void main(int argc, char *argv[])
+{
+
+    return;
+}
